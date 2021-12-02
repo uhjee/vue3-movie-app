4fa91176b9908eb378718f8089fdd8d5b3a8ff70
uhjee <oper1490@gmail.com>
2021-12-03 00:53:34 +0900

    [vue3-movie-app] FE 초격차 강의
    - jest :: global method

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,15 +1,34 @@
 import { double } from './example';
 
-test('첫 테스트', () => {
-  expect(123).toBe(123);
-});
-// tip. 일부러 틀린 데이터를 먼저 입력
-test('인수가 숫자 데이터 입니다', () => {
-  expect(double(3)).toBe(6);
-  expect(double(10)).toBe(20);
-});
+// describe :: 일종의 테스트 들의 그룹
+describe('그룹 1', () => {
+  beforeAll(() => {
+    console.log('before All');
+  });
+  afterAll(() => {
+    console.log('after All');
+  });
+  beforeEach(() => {
+    console.log('before each');
+  });
+  afterEach(() => {
+    console.log('after each');
+  });
 
-test('인수가 없습니다.', () => {
-  expect(double()).toBe(0);
-  expect(double()).toBe(123);
+  test('첫 테스트', () => {
+    console.log('First Test!');
+    expect(123).toBe(123);
+  });
+  // tip. 일부러 틀린 데이터를 먼저 입력
+  test('인수가 숫자 데이터 입니다', () => {
+    console.log('Second Test!');
+    expect(double(3)).toBe(6);
+    expect(double(10)).toBe(20);
+  });
+
+  test('인수가 없습니다.', () => {
+    console.log('Third Test!');
+    expect(double()).toBe(0);
+    // expect(double()).toBe(123);
+  });
 });
