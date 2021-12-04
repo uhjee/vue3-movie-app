fd599e74b1025fb9fbc6f6c11cf234ce2ba8688d
uhjee <oper1490@gmail.com>
2021-12-04 15:59:39 +0900

    jest - async test

--- tests/example.js
+++ tests/example.js
@@ -2,3 +2,11 @@ export function double(num) {
   if (!num) return 0;
   return num * 2;
 }
+
+// test용 비동기 함수
+export const asyncFn = () =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve('done');
+    }, 2000);
+  });
