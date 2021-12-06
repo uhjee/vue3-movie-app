cfc4f126536df0a7fc975b5ae6cbb5aa11351fab
uhjee <oper1490@gmail.com>
2021-12-06 19:00:48 +0900

    vue-test-utilswrapper.mount() - shallowMount()

--- tests/Parent.test.js
+++ tests/Parent.test.js
@@ -1,11 +1,13 @@
 import { mount, shallowMount } from '@vue/test-utils';
 import Parent from './Parent';
 
+// ! mount :: 깊은 연결
 test('Mount', () => {
   const wrapper = mount(Parent);
   expect(wrapper.html()).toBe('');
 });
 
+// ! shallowMount :: 얉은 연결
 test('ShallowMount', () => {
   const wrapper = shallowMount(Parent);
   expect(wrapper.html()).toBe('');
